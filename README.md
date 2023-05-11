1fcec7c5c3c5f7b7bdd1a06a3e0d3869c7a175a8
brambozz <brambo5@live.nl>
2023-05-11 18:42:53 +0200

    Add script to simulate labeled/unlabeled

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ugssl
 Uncertainty-guided semi-supervised learning
+
+## Prepare simulated dataset
+
+```sh
+python -m ugssl.dataset_preparation.simulate_unlabeled Dataset100_amos Dataset200_amos_10_iteration_0 --percentage_labeled 10
+```
---
+++ src/ugssl/dataset_preparation/simulate_unlabeled.py
@@ -0,0 +1,72 @@
+"""Simulated a labeled+unlabeled situation, given a fully labeled dataset.
+
+This assumes the source dir looks like:
+
+source_dir/
+    |
+    imagesTr/
+    labelsTr/
+
+and will write to dest_dir as:
+
+dest_dir/
+    |
+    imagesTr/
+    labelsTr/
+    imagesUl/
+
+"""
+from argparse import ArgumentParser
+from pathlib import Path
+import random
+import shutil
+import json
+from tqdm import tqdm
+
+
+def main(source_dir: Path, dest_dir: Path, percentage_labeled: int):
+    if not dest_dir.is_dir():
+        print("Destination directory does not exist, making it now.")
+        dest_dir.mkdir(parents=True, exist_ok=True)
+
+    labeled_dest_dir = dest_dir / "imagesTr"
+    labeled_label_dest_dir = dest_dir / "labelsTr"
+    unlabeled_dest_dir = dest_dir / "imagesUl"
+    labeled_dest_dir.mkdir(exist_ok=True)
+    labeled_label_dest_dir.mkdir(exist_ok=True)
+    unlabeled_dest_dir.mkdir(exist_ok=True)
+
+    # Get all files in source dir
+    files = list((source_dir / "imagesTr").glob("*.nii.gz"))
+    print(f"Found {len(files)} cases")
+
+    to_select = int((percentage_labeled / 100) * len(files))
+    print(f"Selecting {to_select} cases for labeled subset")
+
+    # Copy image and labels
+    selected_files = random.sample(files, to_select)
+    for image_path in tqdm(files, desc="Copying files"):
+        label_name = image_path.name.replace("_0000", "")
+        label_path = source_dir / "labelsTr" / label_name
+        if image_path in selected_files:
+            shutil.copyfile(image_path, labeled_dest_dir / image_path.name)
+            shutil.copyfile(label_path, labeled_label_dest_dir / label_path.name)
+        else:
+            shutil.copyfile(image_path, unlabeled_dest_dir / image_path.name)
+
+    # Copy dataset.json and update number of training cases
+    shutil.copyfile(source_dir / "dataset.json", dest_dir / "dataset.json")
+    with open(dest_dir / "dataset.json") as json_file:
+        dataset_json = json.load(json_file)
+    dataset_json["numTraining"] = to_select
+    with open(dest_dir / "dataset.json", "w") as json_file:
+        json.dump(dataset_json, json_file, indent=4)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("source_dir", type=str)
+    parser.add_argument("dest_dir", type=str)
+    parser.add_argument("--percentage_labeled", type=int, default=10)
+    args = parser.parse_args()
+    main(Path(args.source_dir), Path(args.dest_dir), args.percentage_labeled)
